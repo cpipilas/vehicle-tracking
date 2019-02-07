ab91209cb6cad2ace2887b5015d9856ef79143a4
cpipilas <30462769+cpipilas@users.noreply.github.com>
2019-02-07 12:56:37 +0200

    Create vehicle-tracking.cpp

---
+++ firmware/vehicle-tracking.cpp
@@ -0,0 +1,20 @@
+// vehicle tracking application
+
+#include "application.h"
+#include "libs/geolocator/src/google-maps-device-locator.h"
+
+PRODUCT_ID(PLATFORM_ID);
+PRODUCT_VERSION(3);
+SYSTEM_MODE(AUTOMATIC);
+
+GoogleMapsDeviceLocator locator;
+
+void setup() {
+  Serial.begin(9600);
+  // Issue a Wi-Fi scan request and send the results to the cloud every 30 secs
+  locator.withLocatePeriodic(30);
+}
+
+void loop() {
+  locator.loop();
+}
