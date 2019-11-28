e55918bbb4b3be4b1a061e412bb13617a3c06049
cpipilas <30462769+cpipilas@users.noreply.github.com>
2019-11-28 13:47:57 +0200

    Update tests.sh

--- tests.sh
+++ tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# purticle ota update
+# particle ota update
 curl -X PUT "https://api.particle.io/v1/devices/$DEVICE_ID?access_token=$ACCESS_TOKEN" -F file=@firmware/modules/particleGeoLoc/target/particleGeoLoc.bin -F file_type=binary
